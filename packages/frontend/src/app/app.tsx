6c13e5c958d5d66c73804bdc34bff29a82fd14fb
Nevo David <nevo@notu.co>
2022-10-17 22:18:01 +0700

    feat: eslint

--- packages/frontend/src/app/app.tsx
+++ packages/frontend/src/app/app.tsx
@@ -1,32 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
 
-export function App() {
-  return (
-    <>
-      <NxWelcome title="frontend" />
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
+export const App = () => (
+    <Routes>
         <Route
           path="/"
           element={
@@ -45,9 +21,6 @@ export function App() {
           }
         />
       </Routes>
-      {/* END: routes */}
-    </>
-  );
-}
+  )
 
 export default App;
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,16 +1,22 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
